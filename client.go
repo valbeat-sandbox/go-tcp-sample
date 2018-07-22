7ec4539c9df3b072435885fa5faade3046592abe
valbeat <kj1ktk@gmail.com>
2018-07-22 23:55:30 +0900

    Change HTTP/1.1

--- client.go
+++ client.go
@@ -6,26 +6,56 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"メッセージ1",
+		"メッセージ2",
+		"メッセージ3",
 	}
-	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
-	if err != nil {
-		panic(err)
+	current := 0
+	var conn net.Conn
+	var err error
+	for {
+		// リトライ用にループ内でコネクション
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		// リクエストを作成
+		request, err := http.NewRequest(
+			"GET",
+			"http://localhost:8888",
+			strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		// レスポンスを読み込み
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+		// 結果を表示
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		// 全部送信済み
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 	}
-
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
 }
--- server.go
+++ server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -25,32 +27,49 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		go func()() {()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+		go func() {
 			// リクエストの処理はノンブロッキング
-			// レスポンスの読み込み
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
-			}
-			// dumpは便利なデバッグ用関数
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(dump))
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			for {
+				// タイムアウトの設定
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				// リクエストの読み込み
+				request, err := http.ReadRequest(bufio.NewReader(conn))
+				if err != nil {
+					// タイムアウトもしくはソケットクローズ時は終了
+					neterr, ok := err.(net.Error)
+					if ok && neterr.Timeout() {
+						fmt.Println("Timeout")
+						break
+					} else if err == io.EOF {
+						fmt.Println("EOF")
+						break
+					}
+					// それ以外はエラー
+					panic(err)
+				}
+				// リクエスト表示
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(string(dump))
 
-			// レスポンスの書き込み
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
-			}
+				content := "Hello World\n"
+				// レスポンスの書き込み
+				// HTTP/1.1 (major1, minor1)
+				// ContentLengthの指定必須
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
+					ContentLength: int64(len(content)),
+					Body:          ioutil.NopCloser(strings.NewReader(content)),
+				}
 
-			response.Write(conn)
+				response.Write(conn)
+			}
 			conn.Close()
 		}()
 	}
-
 }
